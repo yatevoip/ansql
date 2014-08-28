7a0fb043d0c43044657cd915ad6a93a58edc0474
monica <monica@dbfed7de-b0aa-0410-b6a1-c7e608b77fc9>
2014-08-28 11:02:23 +0000

    Fixed undefined object warning.
    git-svn-id: http://voip.null.ro/svn/ansql@113 dbfed7de-b0aa-0410-b6a1-c7e608b77fc9

--- framework.php
+++ framework.php
@@ -1524,9 +1524,9 @@ class Model
 			else
 				$update_log .= "$var_name=***";
 		}
+		$obj_name = $this->getObjectName();
 		if ($variables == "")
 			return array(true, _('Nothing to update in ')._($obj_name).".",array());
-		$obj_name = $this->getObjectName();
 		if($error != "")
 			return array(false,_("Failed to update").' '._($obj_name).".".$error, $error_fields,0);
 		$table = $this->getTableName();
