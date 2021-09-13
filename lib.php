868a2fbbc7c7f0dc7691e4190b0fbe4dc5dd3f53
dana <dana@dbfed7de-b0aa-0410-b6a1-c7e608b77fc9>
2021-09-13 02:25:53 +0000

    Add functions to calculate differences between two dates.
    git-svn-id: http://voip.null.ro/svn/ansql@637 dbfed7de-b0aa-0410-b6a1-c7e608b77fc9

--- lib.php
+++ lib.php
@@ -7781,4 +7781,158 @@ function is_cli()
 		return true;
 	return false;
 }
+
+/**
+ * Calculate differences between two dates with precise semantics. Based on PHPs DateTime::diff()
+ * implementation by Derick Rethans. Ported to PHP by Emil H, 2011-05-02. No rights reserved.
+ * 
+ * See here for original code:
+ * http://svn.php.net/viewvc/php/php-src/trunk/ext/date/lib/tm2unixtime.c?revision=302890&view=markup
+ * http://svn.php.net/viewvc/php/php-src/trunk/ext/date/lib/interval.c?revision=298973&view=markup
+ */
+function _date_range_limit($start, $end, $adj, $a, $b, &$result)
+{
+    if ($result[$a] < $start) {
+        $result[$b] -= intval(($start - $result[$a] - 1) / $adj) + 1;
+        $result[$a] += $adj * intval(($start - $result[$a] - 1) / $adj + 1);
+    }
+	
+    if ($result[$a] >= $end) {
+        $result[$b] += intval($result[$a] / $adj);
+        $result[$a] -= $adj * intval($result[$a] / $adj);
+    }
+
+    return $result;
+}
+
+function _date_range_limit_days(&$base, &$result)
+{
+    $days_in_month_leap = array(31, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+    $days_in_month = array(31, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+
+    _date_range_limit(1, 13, 12, "m", "y", $base);
+
+    $year = $base["y"];
+    $month = $base["m"];
+
+    if (!$result["invert"]) {
+        while ($result["d"] < 0) {
+            $month--;
+            if ($month < 1) {
+                $month += 12;
+                $year--;
+            }
+
+            $leapyear = $year % 400 == 0 || ($year % 100 != 0 && $year % 4 == 0);
+            $days = $leapyear ? $days_in_month_leap[$month] : $days_in_month[$month];
+
+            $result["d"] += $days;
+            $result["m"]--;
+        }
+    } else {
+        while ($result["d"] < 0) {
+            $leapyear = $year % 400 == 0 || ($year % 100 != 0 && $year % 4 == 0);
+            $days = $leapyear ? $days_in_month_leap[$month] : $days_in_month[$month];
+
+            $result["d"] += $days;
+            $result["m"]--;
+
+            $month++;
+            if ($month > 12) {
+                $month -= 12;
+                $year++;
+            }
+        }
+    }
+
+    return $result;
+}
+
+function _date_normalize(&$base, &$result)
+{
+    $result = _date_range_limit(0, 60, 60, "s", "i", $result);
+    $result = _date_range_limit(0, 60, 60, "i", "h", $result);
+    $result = _date_range_limit(0, 24, 24, "h", "d", $result);
+    $result = _date_range_limit(0, 12, 12, "m", "y", $result);
+
+    $result = _date_range_limit_days($base, $result);
+
+    $result = _date_range_limit(0, 12, 12, "m", "y", $result);
+
+    return $result;
+}
+
+/**
+ * Accepts two unix timestamps.
+ */
+function _date_diff($one, $two)
+{
+    $invert = false;
+    if ($one > $two) {
+        list($one, $two) = array($two, $one);
+        $invert = true;
+    }
+
+    $key = array("y", "m", "d", "h", "i", "s");
+    $a = array_combine($key, array_map("intval", explode(" ", date("Y m d H i s", $one))));
+    $b = array_combine($key, array_map("intval", explode(" ", date("Y m d H i s", $two))));
+
+    $result = array();
+    $result["y"] = $b["y"] - $a["y"];
+    $result["m"] = $b["m"] - $a["m"];
+    $result["d"] = $b["d"] - $a["d"];
+    $result["h"] = $b["h"] - $a["h"];
+    $result["i"] = $b["i"] - $a["i"];
+    $result["s"] = $b["s"] - $a["s"];
+    $result["invert"] = $invert ? 1 : 0;
+    $result["days"] = intval(abs(($one - $two)/86400));
+
+    if ($invert) {
+        _date_normalize($a, $result);
+    } else {
+        _date_normalize($b, $result);
+    }
+
+    return $result;
+}
+
+function date_difference($start)
+{
+	if (!class_exists('DateTime')) {
+
+		$res = _date_diff($start, time());
+		if ($res["y"]!=0)
+			return $res["y"]."y ".$res["m"]."m ". $res["d"]."d";
+		if ($res["m"]!=0)
+			return $res["m"]."m ". $res["d"]."d ". $res["h"]."h";
+		if ($res["d"]!=0)
+			return $res["d"]."d ". $res["h"]."h ". $res["i"]."m";
+		if ($res["h"]!=0)
+			return $res["h"]."h ". $res["i"]."m ". $res["s"]."s";
+		if ($res["i"]!=0)
+			return $res["i"]."m ". $res["s"]."s";
+
+		return $res["s"]."s";
+	}
+
+	$start  = date('Y-m-d H:i:s',$start);
+	$end    = date('Y-m-d H:i:s');
+	$d_start    = new DateTime($start);
+	$d_end      = new DateTime($end);
+	$diff = $d_start->diff($d_end);
+
+	if ($diff->format('%y'))
+		return $diff->format('%y'). "y ". $diff->format('%m'). "m ". $diff->format('%d')."d";
+	elseif ($diff->format('%m'))
+		return $diff->format('%m'). "m ". $diff->format('%d')."d ". $diff->format('%h')."h";
+	elseif ($diff->format('%d'))
+		return$diff->format('%d')."d ". $diff->format('%h')."h ". $diff->format('%i')."m";
+	elseif ($diff->format('%h'))
+		return $diff->format('%h')."h ". $diff->format('%i')."m ". $diff->format('%s')."s";
+	elseif ($diff->format('%i'))
+		return $diff->format('%i')."m ". $diff->format('%s')."s";
+	else
+		return $diff->format('%s')."s";
+}
+
 ?>
